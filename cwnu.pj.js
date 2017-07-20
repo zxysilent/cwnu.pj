40138ccdee9768b8988efa71e886e8f2de5f9a47
zxysilent <zxysilent@foxmail.com>
2017-07-20 12:38:26 +0800

    update  new version

--- cwnu.pj.js
+++ cwnu.pj.js
@@ -1,1 +1,1 @@
-﻿ (function(){for(var i=1;i<=20;i++){document.getElementsByName('fen'+i)[9].checked=true;}document.getElementsByName('fen21')[0].value='没其他意见';document.getElementById('baojia').submit();})();
+﻿ (function(){for(var i=1;i<=20;i++){document.getElementsByName('fen'+i)[0].checked=true;}document.getElementsByName('fen21')[0].value='没其他意见';document.getElementById('baojia').submit();})();
